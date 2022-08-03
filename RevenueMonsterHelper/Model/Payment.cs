d93c00a41850f42d9b450384e86c82adf017e4d8
CLHuah <huahchunliang@hotmail.com>
2022-08-03 15:23:41 +0800

    - Update to .Net6. - Update data model.

--- RevenueMonsterHelper/Model/Payment.cs
+++ RevenueMonsterHelper/Model/Payment.cs
@@ -2,159 +2,164 @@
 using System;
 using System.Collections.Generic;
 
-namespace RevenueMonsterLibrary.Model
+namespace RevenueMonsterLibrary.Model;
+
+public class ExtraInfo
 {
-    public class ExtraInfo
+    public string reference { get; set; }
+    public string type { get; set; }
+}
+
+public class QuickPay
+{
+    public string authCode { get; set; }
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public ExtraInfo extraInfo { get; set; }
+
+    public string ipAddress { get; set; }
+    public Order order { get; set; }
+
+    public string storeId { get; set; }
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public string terminalId { get; set; }
+}
+
+public class PaymentTransactionByOrderID
+{
+    public string code { get; set; }
+    public Error error { get; set; }
+    public TransactionQuickPay item { get; set; }
+}
+
+public class TransactionQuickPay
+{
+    public long balanceAmount { get; set; }
+    public string createdAt { get; set; }
+    public string currencyType { get; set; }
+    public Error error { get; set; }
+    public string method { get; set; }
+    public Order order { get; set; }
+    public Payee payee { get; set; }
+    public string platform { get; set; }
+    public string referenceId { get; set; }
+    public string region { get; set; }
+    public string status { get; set; }
+    public Store store { get; set; }
+    public string terminalId { get; set; }
+    public string transactionAt { get; set; }
+    public string transactionId { get; set; }
+    public string type { get; set; }
+    public string updatedAt { get; set; }
+}
+
+public class Expiry
+{
+    public float day { get; set; }
+    public string expiredAt { get; set; }
+    public string type { get; set; }
+}
+
+public class Store
+{
+    public string addressLine1 { get; set; }
+    public string addressLine2 { get; set; }
+    public string city { get; set; }
+    public string country { get; set; }
+    public string countryCode { get; set; }
+    public string createdAt { get; set; }
+    public GeoLocation geoLocation { get; set; }
+    public string id { get; set; }
+    public Uri imageUrl { get; set; }
+    public string name { get; set; }
+    public string phoneNumber { get; set; }
+    public string postCode { get; set; }
+    public string state { get; set; }
+    public string status { get; set; }
+    public string updatedAt { get; set; }
+}
+
+public class GeoLocation
+{
+    public decimal latitude { get; set; }
+    public decimal longitude { get; set; }
+}
+
+public class Payee
+{
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public string userId { get; set; }
+}
+
+public class WebPayment
+{
+    public WebPayment()
     {
-        public string reference { get; set; }
-        public string type { get; set; }
+        order = new Order();
     }
 
-    public class QuickPay
-    {
-        public string authCode { get; set; }
-        public ExtraInfo extraInfo { get; set; }
-        public string ipAddress { get; set; }
-        public Order order { get; set; }
-        public string storeId { get; set; }
-    }
+    public string layoutVersion { get; set; } // v1 / v2 (Supported Credit Card)
+    public IList<string> method { get; set; }
 
+    public string notifyUrl { get; set; }
 
-    public class PaymentTransactionByOrderID
-    {
-        public string code { get; set; }
-        public Error error { get; set; }
-        public TransactionQuickPay item { get; set; }
-    }
+    public Order order { get; set; }
 
-    public class TransactionQuickPay
-    {
-        public long balanceAmount { get; set; }
-        public string createdAt { get; set; }
-        public string currencyType { get; set; }
-        public Error error { get; set; }
-        public string method { get; set; }
-        public Order order { get; set; }
-        public Payee payee { get; set; }
-        public string platform { get; set; }
-        public string referenceId { get; set; }
-        public string region { get; set; }
-        public string status { get; set; }
-        public Store store { get; set; }
-        public string terminalId { get; set; }
-        public string transactionAt { get; set; }
-        public string transactionId { get; set; }
-        public string type { get; set; }
-        public string updatedAt { get; set; }
-    }
+    public string redirectUrl { get; set; }
+    public string storeId { get; set; }
 
-    public class Expiry
-    {
-        public float day { get; set; }
-        public string expiredAt { get; set; }
-        public string type { get; set; }
-    }
+    public string type { get; set; } // WEB_PAYMENT or MOBILE_PAYMENT
+}
 
-    public class Store
-    {
-        public string addressLine1 { get; set; }
-        public string addressLine2 { get; set; }
-        public string city { get; set; }
-        public string country { get; set; }
-        public string countryCode { get; set; }
-        public string createdAt { get; set; }
-        public GeoLocation geoLocation { get; set; }
-        public string id { get; set; }
-        public Uri imageUrl { get; set; }
-        public string name { get; set; }
-        public string phoneNumber { get; set; }
-        public string postCode { get; set; }
-        public string state { get; set; }
-        public string status { get; set; }
-        public string updatedAt { get; set; }
-    }
+public class Order
+{
+    public string additionalData { get; set; }
+    public long amount { get; set; }
 
-    public class GeoLocation
-    {
-        public decimal latitude { get; set; }
-        public decimal longitude { get; set; }
-    }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public string currencyType { get; set; }
 
-    public class Payee
-    {
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-        public string userId { get; set; }
-    }
+    public string detail { get; set; }
+    public string id { get; set; }
+    public string title { get; set; }
+}
 
-    public class WebPayment
-    {
-        public WebPayment()
-        {
-            order = new Order();
-        }
+public class WebPaymentResponse
+{
+    public string code { get; set; }
+    public Item item { get; set; }
+}
 
-        public string layoutVersion { get; set; } // v1 / v2 (Supported Credit Card)
-        public IList<string> method { get; set; }
+public class Item
+{
+    public string checkoutId { get; set; }
+    public string url { get; set; }
+}
 
-        public string notifyUrl { get; set; }
+public class Notify
+{
+    public Data data { get; set; }
+    public string eventType { get; set; }
+}
 
-        public Order order { get; set; }
-
-        public string redirectUrl { get; set; }
-        public string storeId { get; set; }
-
-        public string type { get; set; } // WEB_PAYMENT or MOBILE_PAYMENT
-    }
-
-    public class Order
-    {
-        public string additionalData { get; set; }
-        public long amount { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-        public string currencyType { get; set; }
-
-        public string detail { get; set; }
-        public string id { get; set; }
-        public string title { get; set; }
-    }
-
-    public class WebPaymentResponse
-    {
-        public string code { get; set; }
-        public Item item { get; set; }
-    }
-
-    public class Item
-    {
-        public string checkoutId { get; set; }
-        public string url { get; set; }
-    }
-
-    public class Notify
-    {
-        public Data data { get; set; }
-        public string eventType { get; set; }
-    }
-
-    public class Data
-    {
-        public long balanceAmount { get; set; }
-        public string createdAt { get; set; }
-        public string currencyType { get; set; }
-        public string method { get; set; }
-        public Order order { get; set; }
-        public Payee payee { get; set; }
-        public string platform { get; set; }
-        public string referenceId { get; set; }
-        public string region { get; set; }
-        public string status { get; set; }
-        public Store store { get; set; }
-        public string terminalId { get; set; }
-        public string transactionAt { get; set; }
-        public string transactionId { get; set; }
-        public string type { get; set; }
-        public string updatedAt { get; set; }
-        public Voucher voucher { get; set; }
-    }
+public class Data
+{
+    public long balanceAmount { get; set; }
+    public string createdAt { get; set; }
+    public string currencyType { get; set; }
+    public string method { get; set; }
+    public Order order { get; set; }
+    public Payee payee { get; set; }
+    public string platform { get; set; }
+    public string referenceId { get; set; }
+    public string region { get; set; }
+    public string status { get; set; }
+    public Store store { get; set; }
+    public string terminalId { get; set; }
+    public string transactionAt { get; set; }
+    public string transactionId { get; set; }
+    public string type { get; set; }
+    public string updatedAt { get; set; }
+    public Voucher voucher { get; set; }
 }
--- RevenueMonsterHelper/Model/Voucher.cs
+++ RevenueMonsterHelper/Model/Voucher.cs
@@ -1,77 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-namespace RevenueMonsterLibrary.Model
+namespace RevenueMonsterLibrary.Model;
+
+public class IssueVoucher
 {
-    public class IssueVoucher
-    {
-        public string code { get; set; }
-        public string qrUrl { get; set; }
-    }
+    public string code { get; set; }
+    public string qrUrl { get; set; }
+}
 
-    public class IssueVoucherResult{
-        public IssueVoucher item { get; set; }
-        public string code { get; set; }
-        public Error error { get; set; }
-    }
+public class IssueVoucherResult
+{
+    public string code { get; set; }
+    public Error error { get; set; }
+    public IssueVoucher item { get; set; }
+}
 
-    public class VoidVoucherResult
-    {
-        public Voucher item { get; set; }
-        public string code { get; set; }
-        public Error error { get; set; }
-    }
+public class VoidVoucherResult
+{
+    public string code { get; set; }
+    public Error error { get; set; }
+    public Voucher item { get; set; }
+}
 
-    public class GetVoucherByCodeResult
-    {
-        public Voucher item { get; set; }
-        public string code { get; set; }
-        public Error error { get; set; }
-    }
+public class GetVoucherByCodeResult
+{
+    public string code { get; set; }
+    public Error error { get; set; }
+    public Voucher item { get; set; }
+}
 
-    public class GetVoucherBatchesResult
-    {
-        public List<Voucher> items { get; set; }
-        public string code { get; set; }
-        public Error error { get; set; }
-    }
+public class GetVoucherBatchesResult
+{
+    public string code { get; set; }
+    public Error error { get; set; }
+    public List<Voucher> items { get; set; }
+}
 
-    public class GetVoucherBatchByKeyResult
-    {
-        public Voucher item { get; set; }
-        public string code { get; set; }
-        public Error error { get; set; }
-    }
+public class GetVoucherBatchByKeyResult
+{
+    public string code { get; set; }
+    public Error error { get; set; }
+    public Voucher item { get; set; }
+}
 
-    public class Voucher
-    {
-        public string id { get; set; }
-        public string key { get; set; }
-        public string label { get; set; }
-        public string redemptionRuleKey { get; set; }
-        public string voucherBatchKey { get; set; }
-        public string type { get; set; }
-        public Int64 quantity { get; set; }
-        public Int64 balanceQuantity { get; set; }
-        public Int64 usedQuantity { get; set; }
-        public Int64 amount { get; set; }
-        public Int64 discountRate { get; set; }
-        public Int64 minimumSpendAmount { get; set; }
-        public string origin { get; set; }
-        public string imageUrl { get; set; }
-        public string memberProfile { get; set; }
-        public string assignedAt { get; set; }
-        public string payload { get; set; }
-        public string qrUrl { get; set; }
-        public string code { get; set; }
-        public bool isShipping { get; set; }
-        public Address address { get; set; }
-        public Expiry expiry { get; set; }
-        public string usedAt { get; set; }
-        public string redeemedAt { get; set; }
-        public bool isDeviceRedeem { get; set; }
-        public string status { get; set; }
-        public string createdAt { get; set; }
-        public string updatedAt { get; set; }
-    }
+public class Voucher
+{
+    public Address address { get; set; }
+    public long amount { get; set; }
+    public string assignedAt { get; set; }
+    public long balanceQuantity { get; set; }
+    public string code { get; set; }
+    public string createdAt { get; set; }
+    public long discountRate { get; set; }
+    public Expiry expiry { get; set; }
+    public string id { get; set; }
+    public string imageUrl { get; set; }
+    public bool isDeviceRedeem { get; set; }
+    public bool isShipping { get; set; }
+    public string key { get; set; }
+    public string label { get; set; }
+    public string memberProfile { get; set; }
+    public long minimumSpendAmount { get; set; }
+    public string origin { get; set; }
+    public string payload { get; set; }
+    public string qrUrl { get; set; }
+    public long quantity { get; set; }
+    public string redeemedAt { get; set; }
+    public string redemptionRuleKey { get; set; }
+    public string status { get; set; }
+    public string type { get; set; }
+    public string updatedAt { get; set; }
+    public string usedAt { get; set; }
+    public long usedQuantity { get; set; }
+    public string voucherBatchKey { get; set; }
 }
