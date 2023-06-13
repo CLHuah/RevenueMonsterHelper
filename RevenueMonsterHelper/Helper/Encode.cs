0a3fa54598a1351a4962eec224526de47eac4fba
CLHuah <huahchunliang@hotmail.com>
2023-06-13 18:08:30 +0800

    - Add comment and summary to code
    - Added comment and summary to Decode.cs, Encode.cs and RandomString.cs.

--- RevenueMonsterHelper/Helper/Encode.cs
+++ RevenueMonsterHelper/Helper/Encode.cs
@@ -3,11 +3,22 @@ using System.Text;
 
 namespace RevenueMonsterLibrary.Helper;
 
+/// <summary>
+///     Provides methods for encode a plain text string to Base64.
+/// </summary>
 public static class Encode
 {
+    /// <summary>
+    ///     Encodes a plain text string to Base64.
+    /// </summary>
+    /// <param name="plainText">The plain text to encode.</param>
+    /// <returns>The Base64 encoded string.</returns>
     public static string Base64Encode(string plainText)
     {
+        // Convert the plain text to bytes using UTF-8 encoding
         var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+        // Convert the bytes to Base64 string and return the result
         return Convert.ToBase64String(plainTextBytes);
     }
 }
--- RevenueMonsterHelper/Helper/RandomString.cs
+++ RevenueMonsterHelper/Helper/RandomString.cs
@@ -5,17 +5,28 @@ namespace RevenueMonsterLibrary.Helper;
 
 public class RandomString
 {
-    private static readonly Random Random = new Random((int) DateTime.Now.Ticks);
+    private static readonly Random Random = new((int)DateTime.Now.Ticks);
 
+    /// <summary>
+    ///     Generates a random string of the specified size consisting of uppercase letters.
+    /// </summary>
+    /// <param name="size">The size of the random string to generate.</param>
+    /// <returns>A random string of the specified size.</returns>
     public static string GenerateRandomString(int size)
     {
         var builder = new StringBuilder();
+
+        // Loop to generate individual characters for the random string
         for (var i = 0; i < size; i++)
         {
-            var ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * Random.NextDouble() + 65)));
+            // Generate a random character within the range of uppercase letters (A-Z)
+            var ch = (char)(Random.Next(26) + 'A');
+
+            // Append the generated character to the string builder
             builder.Append(ch);
         }
 
+        // Return the generated random string
         return builder.ToString();
     }
 }
