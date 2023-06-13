274ef3b836230cfdda8f652a5a7ade83983332c6
CLHuah <huahchunliang@hotmail.com>
2023-06-13 18:08:56 +0800

    - Add unit tests
    - Added unit test using MsTest.

---
+++ RevenueMonsterLibrary.Tests/TestCases/DecodeTests.cs
@@ -0,0 +1,19 @@
+﻿namespace RevenueMonsterLibrary.Tests.TestCases;
+
+[TestClass]
+public class DecodeTests
+{
+    [TestMethod]
+    public void Base64Decode_DecodesBase64EncodedString()
+    {
+        // Arrange
+        const string base64EncodedString = "SGVsbG8gV29ybGQh"; // "Hello World!" in base64
+
+        // Act
+        var decodedString = Decode.Base64Decode(base64EncodedString);
+
+        // Assert
+        const string expectedString = "Hello World!";
+        Assert.AreEqual(expectedString, decodedString);
+    }
+}
