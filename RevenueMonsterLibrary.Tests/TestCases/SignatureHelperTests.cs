184fe20a1bbb71eddf3a983ea378921f4b1a7a03
CLHuah <huahchunliang@hotmail.com>
2023-11-16 15:02:41 +0800

    Added SignatureHelper unit test

---
+++ RevenueMonsterLibrary.Tests/TestCases/SignatureHelperTests.cs
@@ -0,0 +1,55 @@
+﻿namespace RevenueMonsterLibrary.Tests.TestCases;
+
+[TestClass]
+public class SignatureHelperTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void GenerateCompactJson_NullInput_ShouldThrowException()
+    {
+        // Act
+        SignatureHelper.GenerateCompactJson(null);
+
+        // Assert
+        // Exception is expected, so nothing to assert explicitly
+    }
+
+    [TestMethod]
+    public void GenerateCompactJson_ShouldReturnSortedJson()
+    {
+        // Arrange
+        var testData = new OuterClass
+        {
+            OuterId = 2,
+            OuterName = "Outer",
+            Inner = new InnerClass
+            {
+                InnerId = 1,
+                InnerName = "Inner"
+            }
+        };
+
+        // Act
+        var result = SignatureHelper.GenerateCompactJson(testData);
+
+        // Assert
+        const string expectedJson =
+            "{\"Inner\":{\"InnerId\":1,\"InnerName\":\"Inner\"},\"OuterId\":2,\"OuterName\":\"Outer\"}";
+
+        Assert.AreEqual(expectedJson, result);
+    }
+
+    // Define a sample class for testing
+    private class InnerClass
+    {
+        public int InnerId { get; set; }
+        public string InnerName { get; set; }
+    }
+
+    private class OuterClass
+    {
+        public InnerClass Inner { get; set; }
+        public int OuterId { get; set; }
+        public string OuterName { get; set; }
+    }
+}
